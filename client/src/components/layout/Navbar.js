5b50932ef34ba577741a57ca59e937217aefadf0
“Jonny <jonny.kelly192@gmail.com>
2021-08-05 15:59:35 +0100

    styling fixes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,8 +12,7 @@ class Navbar extends Component {
                         style={{ fontFamily: "monospace" }}
                         className="col s5 brand-logo center black-text"
                         >
-                            <i className="material-icons">code</i>
-                            Burnout
+                            <h1>BurnOut</h1>
                         </Link>
                     </div>
                 </nav>
